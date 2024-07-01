2668ffa629fdfa8557657c63f5b74c7a49be73bc
SOORIYANARAYANAN-AJ <sooriyanarayanan19@gmail.com>
2024-07-01 20:04:04 +0530

    created npm

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react"
+import ReactDOM from "react-dom";
 const head=React.createElement("h1" , {id:"head"} , "hello");
     const root=ReactDOM.createRoot(document.getElementById("root"));
     root.render(head);
